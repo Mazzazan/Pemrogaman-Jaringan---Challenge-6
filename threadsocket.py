2dbfbebd99f1df42e727bf549d9822a8daf85897
M Yusuf Haidar Khairullah <74356109+EdgeSky-Master@users.noreply.github.com>
2024-05-03 13:16:09 +0700

    1st challenge (revise)

--- threadsocket.py
+++ threadsocket.py
@@ -2,6 +2,8 @@ import socket
 import select
 import sys
 import threading
+import re
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ip_address = '127.0.0.1'
@@ -9,45 +11,52 @@ port = 8081
 server.bind((ip_address, port))
 server.listen(100)
 list_of_client = []
+
 def clientthread(conn, addr):
     while True:
         try:
-            message = conn.recv(2048)
+            message = conn.recv(2048).decode()
             if message:
-                print("<" + addr[0] + ">" + message)
-                message_to_send = "<" + addr[0] + ">" + message
-                for i in message.split():
-                    if i in ['+','-','*','/'] or i.isdigit():
-                        result = 0
+                print(f"Received message from <{addr[0]}>: {message}")
+
+                splitted_message = message.split()
+                for i in splitted_message:
+                    if i in ['+', '-', '*', '/'] or i.isdigit():
+                        result = None
                         operation_list = []
-
-                        for letter in message:
+                        for letter in splitted_message:
                             operation_list.append(letter)
-                        oprnd1 = operation_list[0]
-                        operation = operation_list[1]
-                        oprnd2 = operation_list[2]
+                        operand_1 = operation_list[0]
+                        operator = operation_list[1]
+                        operand_2 = operation_list[2]
+                        
+                        if operator == '+':
+                            result = int(operand_1) + int(operand_2)
+                        elif operator == '-':
+                            result = int(operand_1) - int(operand_2)
+                        elif operator == '*':
+                            result = int(operand_1) * int(operand_2)
+                        elif operator == '/':
+                            result = int(operand_1) / int(operand_2)
+                    if result is not None:
 
-                        num1 = int(oprnd1)
-                        num2 = int(oprnd2)
+                        message_to_send = f"{operand_1}{operator}{operand_2}={result}\n".encode()
+                    else:
+                        message_to_send = "Invalid operation.\n".encode()
+                else:
 
-                        if operation == "+":
-                            result = num1 + num2
-                        elif operation == "-":
-                            result = num1 - num2
-                        elif operation == "/":
-                            result = num1 / num2
-                        elif operation == "*":
-                            result = num1 * num2
+                    message_to_send = message.encode()
+                
 
-
-                        output = str(result)
-                        message = message + "=" + output + "\n"
+                print(f"Sending: {message_to_send.decode()}")
                 broadcast(message_to_send, conn)
 
             else:
                 remove(conn)
-        except:
+        except Exception as e:
+            print(f"An error occurred: {e}")
             continue
+
 def broadcast(message, connection):
     for clients in list_of_client:
         if clients!=connection:
@@ -59,11 +68,11 @@ def broadcast(message, connection):
 def remove(connection):
     if connection in list_of_client:
         list_of_client.remove(connection)
-        
+
 while True:
     conn, addr = server.accept()
     list_of_client.append(conn)
     print(addr[0] + " connected")
     threading.Thread(target=clientthread,args=(conn,addr)).start()
-    
+
 conn.close()
